416b0c485c6e85ab09e748c0e4a634901639a69b
nishu-murmu <nishumurmu017@gmail.com>
2025-07-05 21:43:32 +0530

    feat: adding basic astro project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
+import mdx from '@astrojs/mdx';
+
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), mdx()],
+  adapter: vercel()
+});
