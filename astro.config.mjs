4cc994c5c0550899256bef7ce716143eef178a8a
mithlesh-vishwakarma <mv9898733607@gmail.com>
2026-07-15 12:49:36 +0530

    feat: adding astro-expressive-code for better code snippet experience.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,16 @@ import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
+import expressiveCode from "astro-expressive-code"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     react(),
-    mdx(),
+    expressiveCode({
+      themes: ['solarized-dark'],
+    }),
+    mdx()
   ],
   adapter: vercel({
     webAnalytics: {
